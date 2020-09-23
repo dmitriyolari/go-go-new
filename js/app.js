77e35d3074b7060c2929fb805439fee5c632105d
Дмитрий <dumitrashikeu@gmail.com>
2020-09-23 12:15:06 +0300

    all files added 1

---
+++ js/app.js
@@ -0,0 +1,13 @@
+$(function () {
+
+
+$(".slider-new-tel").slick({
+   arrows: true,
+   infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 3,
+    autoplay: false,
+    speed: 2000
+});
+
+});
