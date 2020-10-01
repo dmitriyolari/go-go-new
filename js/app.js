58490c0d598ee264d14247cb32620042c2b71c80
alexGreat <sashazaleskih@gmail.com>
2020-10-01 10:31:56 +0300

    add slick slider

--- js/app.js
+++ js/app.js
@@ -1,13 +1,20 @@
 $(function () {
 
 
-$(".slider-new-tel").slick({
-   arrows: true,
-   infinite: true,
-    slidesToShow: 1,
+  $(".slider-new-tel-three").slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 3,
     slidesToScroll: 3,
-    autoplay: false,
-    speed: 2000
-});
+    speed: 1000,
+  });
+  $(".slider-new-tel-four").slick({
+    arrows: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    speed: 1000,
+  });
+
 
 });
