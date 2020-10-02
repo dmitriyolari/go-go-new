b19cb5eb0c8c0c61e63c0aca57094d1dffe6142a
Дмитрий <dumitrashikeu@gmail.com>
2020-10-02 11:54:28 +0300

    customized arrows

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 3,
     speed: 1000,
+    prevArrow: '<img class="a-left slick-prev" src="../img/slick-prev.png" alt="">',
+    nextArrow: "<img class='a-right slick-next' src='../img/slick-next.png' alt=''>"
   });
   $(".slider-new-tel-four").slick({
     arrows: true,
