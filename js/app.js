83c3369f31b9c66ffa583adbf1ab8ee9ba13dd06
Дмитрий <dumitrashikeu@gmail.com>
2020-10-05 17:26:11 +0300

    positioned elements in card right width of card

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@ $(function () {
     slidesToScroll: 3,
     speed: 1000,
     prevArrow: '<img class="a-left slick-prev" src="../img/slick-prev.png" alt="">',
-    nextArrow: "<img class='a-right slick-next' src='../img/slick-next.png' alt=''>"
+    nextArrow: "<img class='a-right slick-next' src='../img/slick-next.png' alt=''>",
+    variableWidth: true,
   });
   $(".slider-new-tel-four").slick({
     arrows: true,
@@ -16,6 +17,7 @@ $(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     speed: 1000,
+    variableWidth: true,
   });
 
 
