aacf3cc8023f79a1c4ea0c76393f295f7e5ad8a4
Дмитрий <dumitrashikeu@gmail.com>
2020-10-06 09:46:23 +0300

    add images of arrows

--- js/app.js
+++ js/app.js
@@ -2,17 +2,13 @@ $(function () {
 
 
   $(".slider-new-tel-three").slick({
-    arrows: true,
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
     speed: 1000,
-    prevArrow: '<img class="a-left slick-prev" src="../img/slick-prev.png" alt="">',
-    nextArrow: "<img class='a-right slick-next' src='../img/slick-next.png' alt=''>",
     variableWidth: true,
   });
   $(".slider-new-tel-four").slick({
-    arrows: true,
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4,
