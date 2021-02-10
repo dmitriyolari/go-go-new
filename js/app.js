efcb5d402fc380353f0543052312768ccab344e2
Dmitriy <dumitrashikeu@gmail.com>
2021-02-10 14:31:31 +0200

    remove tabs

--- js/app.js
+++ js/app.js
@@ -15,34 +15,6 @@ $(function () {
         $('.slider-new-tel-two').slick('setPosition');
     });
 
-
-    const tabsBtn   = document.querySelectorAll(".footer-nav-item");
-    const tabsItems = document.querySelectorAll(".tab-pane");
-
-    tabsBtn.forEach(onTabClick);
-
-    function onTabClick(item) {
-        item.addEventListener("click", function() {
-            let currentBtn = item;
-            let tabId = currentBtn.getAttribute("data-tab");
-            let currentTab = document.querySelector(tabId);
-
-            if ( ! currentBtn.classList.contains('active') ) {
-                tabsBtn.forEach(function (item) {
-                    item.classList.remove('active');
-                });
-
-                tabsItems.forEach(function (item) {
-                    item.classList.remove('active');
-                });
-
-                currentBtn.classList.add('active');
-                currentTab.classList.add('active');
-            }
-
-        });
-    };
-
 });
 
 
